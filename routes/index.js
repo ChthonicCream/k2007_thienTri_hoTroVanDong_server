003198cf731df4e6c9430824dc9f3ea82acfbf2d
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-01-02 13:15:19 +0700

    update path for idnex

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const path = require("path");
+
+const indexFile = path.join(__dirname, "../public/views/index copy.html");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('./views/index copy.html', { root: 'public' });
+  res.sendFile(indexFile);
 });
 
 module.exports = router;
